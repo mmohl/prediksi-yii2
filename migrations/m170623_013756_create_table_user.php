ab68aa837821bfe08e2419eeb86c1c1253c70c81
mmohl <mmohammadl.lpkia@gmail.com>
2017-06-26 06:56:35 +0700

    add auth_key and access token

--- migrations/m170623_013756_create_table_user.php
+++ migrations/m170623_013756_create_table_user.php
@@ -26,8 +26,10 @@ class m170623_013756_create_table_user extends Migration {
             'username' => $this->string(20)->notNull(), // varchar(20) NOT NULL,
             'password' => $this->string(255)->notNull(), // varchar(255) NOT NULL,
             'isActive' => $this->char(1)->defaultValue('1'),
+            'auth_key' => $this->string(255)->null(),
+            'access_token' => $this->string(255)->null(),
             'created_at' => $this->integer(11)->notNull(),
-            'updated_at' => $this->integer(11)->notNull(),
+            'updated_at' => $this->integer(11)->null(),
         ]);
     }
 
