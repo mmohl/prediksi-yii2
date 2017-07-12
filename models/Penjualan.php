196109c2a03acde358fa608c50aa5735be3d43e6
mmohl <mmohammadl.lpkia@gmail.com>
2017-07-13 06:55:54 +0700

    configure penjualan class

--- models/Penjualan.php
+++ models/Penjualan.php
@@ -12,8 +12,12 @@ use Yii;
  * @property string $tahun
  * @property string $bulan
  * @property integer $jumlah
+ * @property integer index
  */
 class Penjualan extends Model {
+
+    public $index;
+    public $kwadrat;
 
     /**
      * @inheritdoc
@@ -27,10 +31,11 @@ class Penjualan extends Model {
      */
     public function rules() {
         return [
-            [['id_teknik', 'jumlah'], 'integer'],
+            [['id_teknik', 'jumlah', 'kwadrat', 'index'], 'integer'],
             [['tahun', 'bulan'], 'required'],
             [['tahun'], 'string', 'max' => 10],
             [['bulan'], 'string', 'max' => 50],
+            [['index', 'kwadrat'], 'safe']
         ];
     }
 
@@ -44,6 +49,47 @@ class Penjualan extends Model {
             'tahun' => Yii::t('app', 'Tahun'),
             'bulan' => Yii::t('app', 'Bulan'),
             'jumlah' => Yii::t('app', 'Jumlah'),
+            'index' => \Yii::t('app', 'Index')
+        ];
+    }
+
+    public function setIndex($value) {
+        $this->index = $value;
+    }
+
+    public function getIndex() {
+        return $this->index;
+    }
+
+    public static function getAllYears($limit = null) {
+
+        $tmp = Penjualan::find()->select(['tahun'])->distinct();
+
+        if (!empty($limit)) {
+            $tmp->where(['<', 'tahun', $limit]);
+        }
+
+        $years = $tmp->asArray()->all();
+
+        return array_map(function($year) {
+            return $year['tahun'];
+        }, $years);
+    }
+
+    public static function getMonths() {
+        return [
+            'Januari',
+            'Februari',
+            'Maret',
+            'April',
+            'Mei',
+            'Juni',
+            'Juli',
+            'Agustus',
+            'September',
+            'Oktober',
+            'November',
+            'Desember'
         ];
     }
 
