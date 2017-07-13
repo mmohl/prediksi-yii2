796c1f3533673fc3cfea1c60bf2a708077034acf
mmohl <mmohammadl.lpkia@gmail.com>
2017-07-13 09:55:23 +0700

    defines all logic methods

--- models/Prediksi.php
+++ models/Prediksi.php
@@ -19,6 +19,7 @@ class Prediksi extends \yii\base\Model {
     public $tahun;
     public $isYear = false;
     public $isOdd;
+    private $source;
 
     public function rules() {
         return [
@@ -52,17 +53,16 @@ class Prediksi extends \yii\base\Model {
 
     public function calculate() {
         $source = $this->group($this->getSource($this->tahun));
-        $sources = null;
-
+        $tmp = null;
         $this->isOdd = count($source) % 2 == 1 ? true : false;
 
         if ($this->isOdd) {
-            $sources = $this->isOdd($source);
+            $tmp = $this->isOdd($source);
         } else {
-            $sources = $this->isEven($source);
+            $tmp = $this->isEven($source);
         }
 
-        return $this->countKwadrat($sources);
+        $this->source = $this->countKwadrat($tmp);
     }
 
     public function getSource($year) {
@@ -145,6 +145,142 @@ class Prediksi extends \yii\base\Model {
         }
 
         return $datas;
+    }
+
+    public function getHeaders() {
+        $data = [];
+        foreach ($this->source[0] as $key => $value) {
+            $data[] = $key;
+        }
+
+        return $data;
+    }
+
+    public function getSources() {
+        return $this->source;
+    }
+
+    public function getFooters() {
+        $headers = $this->getHeaders();
+        $data = [];
+
+        for ($k = 2; $k < count($headers); $k++) {
+            $total = 0;
+            foreach ($this->source as $sourc) {
+                $total = $total + $sourc[$headers[$k]];
+            }
+            $data[$headers[$k]] = $total;
+        }
+
+        return $data;
+    }
+
+    public function getNextMonth() {
+        $months = Prediksi::getMonths();
+        $last = end($this->source);
+        $data = null;
+
+        if (strtolower($last['bulan']) === 'desember') {
+            $data['tahun'] = intval($last['tahun']) + 1;
+            $data['bulan'] = 'januari';
+        } else {
+            array_search($months[strtolower($last['bulan'])] + 1, $months);
+        }
+
+        if ($this->isOdd) {
+            $data['x'] = $last['x'] + 1;
+        } else {
+            $data['x'] = $last['x'] + 2;
+        }
+
+        return $data;
+    }
+
+    public function getLiniers() {
+        $datas = [];
+        $total = count($this->source);
+        $footers = $this->getFooters();
+        $kwadrat = $footers['x2'];
+        $excludes = ['x2', 'x'];
+
+        foreach ($footers as $key => $value) {
+            if (!in_array($key, $excludes)) {
+                if (substr($key, 0, 1) === 'x') {
+                    $datas[substr($key, 1, strlen($key))]['2'] = $value / $kwadrat;
+                } else {
+                    $datas[$key]['1'] = $value / $total;
+                }
+            }
+        }
+
+        return $datas;
+    }
+
+    public function getPrediction($order = false) {
+        $datas = [];
+        $liniers = $this->getLiniers();
+        $next = $this->getNextMonth();
+
+        foreach ($liniers as $key => $values) {
+            $datas[$key] = round($values[1] + ($values[2] * $next['x']));
+        }
+
+        if ($order) {
+            uasort($datas, [$this, 'cmp']);
+            return $datas;
+        }
+
+        return $datas;
+    }
+
+    public function getHighest($labeled = false) {
+
+        if ($labeled) {
+
+            $tot = $this->getPrediction(true);
+            $keys = array_keys($tot);
+            $highest = max($tot);
+
+            return ['value' => $highest, 'label' => $keys[count($keys) - 1]];
+        }
+
+        return max($this->getPrediction());
+    }
+
+    public function getLowest($labeled = false) {
+        if ($labeled) {
+            $tot = $this->getPrediction(true);
+            $keys = array_keys($tot);
+            $lowest = min($tot);
+
+            return ['value' => $lowest, 'label' => $keys[0]];
+        }
+
+        return min($this->getPrediction());
+    }
+
+    private function cmp($a, $b) {
+        if ($a == $b) {
+            return 0;
+        }
+        return ($a < $b) ? -1 : 1;
+    }
+
+    private static function getMonths() {
+        return [
+            'januari' => 1,
+            'februari' => 2,
+            'maret' => 3,
+            'april' => 4,
+            'mei' => 5,
+            'juni' => 6,
+            'juli' => 7,
+            'agustus' => 8,
+            'september' => 9,
+            'oktober' => 10,
+            'november' => 11,
+            'desember' => 12
+        ];
     }
 
 }
